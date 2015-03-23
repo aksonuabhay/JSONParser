6d81d36313b87e10025c418798e6803c068703d7
Abhay <aksonuabhay@gitlab.com>
2015-03-23 07:55:50 +0530

    Setup initial JSON Parsing

---
+++ app/src/main/java/com/example/abhay_hp/jsonparser/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.abhay_hp.jsonparser;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+public class MainActivity extends ListActivity {
+    private Context context;
+    private static String url = "http://quiet-stone-2094.herokuapp.com/transactions.json";
+    private static final String SKU = "sku";
+    private static final String AMOUNT = "amount";
+    private static final String CURRENCY = "currency";
+    ArrayList<HashMap<String, String>> jsonlist = new ArrayList<HashMap<String, String>>();
+    ListView lv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new ProgressTask(MainActivity.this).execute();
+    }
+
+    private class ProgressTask extends AsyncTask<String, Void, Boolean> {
+        private ProgressDialog dialog;
+        private ListActivity activity; // private List<Message> messages;
+
+        public ProgressTask(ListActivity activity) {
+            this.activity = activity;
+            context = activity;
+            dialog = new ProgressDialog(context);
+        }
+
+        private Context context;
+
+        protected void onPreExecute() {
+            this.dialog.setMessage("Progress start");
+            this.dialog.show();
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            if (dialog.isShowing()) {
+                dialog.dismiss();
+            }
+            ListAdapter adapter = new SimpleAdapter(context, jsonlist, R.layout.list_item, new String[]{SKU, AMOUNT, CURRENCY}, new int[]{R.id.sku, R.id.amount, R.id.currency});
+            setListAdapter(adapter); // select single ListView item
+            lv = getListView();
+        }
+
+        protected Boolean doInBackground(final String... args) {
+            JSONParser jParser = new JSONParser(); // get JSON data from URL
+            JSONArray json = jParser.getJSONFromUrl(url);
+            for (int i = 0; i < json.length(); i++) {
+                try {
+                    JSONObject c = json.getJSONObject(i);
+                    String sku = c.getString(SKU);
+                    String amount= c.getString(AMOUNT);
+                    String currency = c.getString(CURRENCY);
+                    HashMap<String, String> map = new HashMap<String, String>(); // Add child node to HashMap key & value
+                    map.put(SKU, sku);
+                    map.put(AMOUNT, amount);
+                    map.put(CURRENCY, currency);
+                    jsonlist.add(map);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+    }
+}
